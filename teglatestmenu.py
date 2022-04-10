c58430c1bd671866a1d0b3ecc6aba6cafadf3446
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2022-04-11 01:01:13 +0200

    Téglatest menü befejezése

--- teglatestmenu.py
+++ teglatestmenu.py
@@ -1,113 +1,108 @@
 from tkinter import *
-foablak = Tk()
 
+def nevjegy():
+    abl2 = Toplevel(foablak)
+    uz2 = Message(abl2, text = "Készítette: Mészáros Richárd")
+    gomb2 = Button(abl2, text = "Kilép", command = abl2.destroy)
+    uz2.pack()
+    gomb2.pack()
+    abl2.mainloop()
 
-#névjegy
+def felszin():
+    def szamit():
+        a = eval(mezo1.get())
+        b = eval(mezo2.get())
+        c = eval(mezo3.get())
+        felszin = 2*(a*b+a*c+b*c)
+        mezo4.delete(0, END)
+        mezo4.insert(0,str(felszin))
 
-def nevjegyablak():
-    nevjegy = Tk()
+    abl3 = Toplevel(foablak)
+    abl3.title("Téglatest felszíne")
+    abl3.minsize(width = 300, height = 100)
+    szoveg1 = Label(abl3, text = "a:")
+    szoveg2 = Label(abl3, text = "b:")
+    szoveg3 = Label(abl3, text = "c:")
+    szoveg4 = Label(abl3, text = "Eredmény:")
     
-    uzenet = Message(nevjegy, text = "Készítette: Valaki \n 2022.04.05", width = 300)
-    uzenet.grid(column = 1, row = 1)
+    gomb1 = Button(abl3, text = "Számít", command= szamit)
 
-    gomb2 = Button(nevjegy, text = "Kilépés", command = nevjegy.destroy)
-    gomb2.grid(column = 1, row = 6)
+    mezo1 = Entry(abl3)
+    mezo2 = Entry(abl3)
+    mezo3 = Entry(abl3)
+    mezo4 = Entry(abl3)
+    szoveg1.grid(row = 1)
+    szoveg2.grid(row = 2)
+    szoveg3.grid(row = 3)
+    szoveg4.grid(row = 5)
+    
+    gomb1.grid(row = 4, column = 2, sticky = W)
 
-    nevjegy.mainloop()
-
-
-#térfogat számítás
-
-def terfogatszamitas():
-    terfogatablak = Tk()
-
-    terfogatszoveg = Label(terfogatablak, text = "Térfogat (cm3): ")
-    terfogatszoveg.grid(column = 0, row = 0)
-    terfogat = Entry(terfogatablak)
-    terfogat.grid(column = 1, row = 0)
-
-    A = Entry(terfogatablak)
-    A.grid(column = 1, row = 2)
-
-    B = Entry(terfogatablak)
-    B.grid(column = 1, row = 3)
-
-    C = Entry(terfogatablak)
-    C.grid(column = 1, row = 4)
-
-    kiszamit = Button(terfogatablak, text = "Kiszámítás", command = szamitas)
-    kiszamit.grid(column = 1, row = 5)
-
-    eredmeny = Entry(terfogatablak)
-    eredmeny.grid(column = 1, row = 6)
-
-    def szamitas():
-        Aadat = int(A.get())
-        Badat = int(B.get())
-        Cadat = int(C.get())
-        terfogatkiszamitas = Aadat * Badat * Cadat
-        terfogat.insert(0, terfogatkiszamitas)
+    mezo1.grid(row = 1, column = 2, sticky = W)
+    mezo2.grid(row = 2, column = 2, sticky = W)
+    mezo3.grid(row = 3, column = 2, sticky = W)
+    mezo4.grid(row = 5, column = 2, sticky = W)
+    
+    abl3.mainloop()
 
 
 
-    terfogatablak.mainloop()
+def terfogat():
+    def szamit():
+        a = eval(mezo1.get())
+        b = eval(mezo2.get())
+        c = eval(mezo3.get())
+        terfogat = a*b*c
+        mezo4.delete(0, END)
+        mezo4.insert(0,str(terfogat))
 
+    abl3 = Toplevel(foablak)
+    abl3.title("Téglatest térfogata")
+    abl3.minsize(width = 300, height = 100)
+    szoveg1 = Label(abl3, text = "a:")
+    szoveg2 = Label(abl3, text = "b:")
+    szoveg3 = Label(abl3, text = "c:")
+    szoveg4 = Label(abl3, text = "Eredmény:")
+    
+    gomb1 = Button(abl3, text = "Számít", command= szamit)
 
-#felszín számítás
+    mezo1 = Entry(abl3)
+    mezo2 = Entry(abl3)
+    mezo3 = Entry(abl3)
+    mezo4 = Entry(abl3)
+    szoveg1.grid(row = 1)
+    szoveg2.grid(row = 2)
+    szoveg3.grid(row = 3)
+    szoveg4.grid(row = 5)
+    
+    gomb1.grid(row = 4, column = 2, sticky = W)
 
-def felszinszamitas():
-    felszinablak = Tk()
-    felszinszoveg = Label(felszinablak, text = "Felszín (cm2): ")
-    felszinszoveg.grid(column = 0, row = 1)
-    felszin = Entry(felszinablak)
-    felszin.grid(column = 1, row = 1)
+    mezo1.grid(row = 1, column = 2, sticky = W)
+    mezo2.grid(row = 2, column = 2, sticky = W)
+    mezo3.grid(row = 3, column = 2, sticky = W)
+    mezo4.grid(row = 5, column = 2, sticky = W)
+    
+    abl3.mainloop()
 
-    A = Entry(felszinablak)
-    A.grid(column = 1, row = 2)
-
-    B = Entry(felszinablak)
-    B.grid(column = 1, row = 3)
-
-    C = Entry(felszinablak)
-    C.grid(column = 1, row = 4)
-
-
-
-    Aadat = int(A.get())
-    Badat = int(B.get())
-    Cadat = int(C.get())
-
-    felszinkiszamitas = 2 * (Aadat * Badat) * (Aadat * Cadat) * (Badat * Cadat) / 100
-    felszin.insert(0, felszinkiszamitas)
-
-    kiszamit = Button(felszinablak, text = "Kiszámítás")
-    kiszamit.grid(column = 1, row = 5)
-
-    felszin.mainloop()
-
-
-#főablak
-
+foablak = Tk()
 foablak.title("A téglatest adatai")
 foablak.minsize(width = 300, height = 100)
 
 menusor = Frame(foablak)
-menusor.pack(side = TOP, fill = X)
+menusor.pack(side= TOP, fill = X)
 
 menu1 = Menubutton(menusor, text = "Fájl", underline = 0)
 menu1.pack(side = LEFT)
 fajl = Menu(menu1)
-fajl.add_command(label = "Névjegy", command = nevjegyablak, underline = 0)
-fajl.add_command(label = "Kilépés", command = foablak.destroy, underline = 0)
+fajl.add_command(label = "Névjegy", command = nevjegy, underline = 0)
+fajl.add_command(label = "Kiépés", command = foablak.destroy, underline = 0)
 menu1.config(menu = fajl)
 
 menu2 = Menubutton(menusor, text = "Téglatest", underline = 0)
 menu2.pack(side = LEFT)
 teglatest = Menu(menu2)
-teglatest.add_command(label = "Felszin", command = felszinszamitas, underline = 0)
-teglatest.add_command(label = "Térfogat", command = terfogatszamitas, underline = 0)
+teglatest.add_command(label = "Felszín", command = felszin, underline = 0)
+teglatest.add_command(label = "Térfogat", command = terfogat, underline = 0)
 menu2.config(menu = teglatest)
-
-
 
 foablak.mainloop()
