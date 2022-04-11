3f7daa947363b940cd32375773200242e3519154
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2022-04-11 13:56:40 +0200

    Téglatest menü kilépés gombok

--- teglatestmenu.py
+++ teglatestmenu.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+
+#névjegy
+
 def nevjegy():
     abl2 = Toplevel(foablak)
     uz2 = Message(abl2, text = "Készítette: Mészáros Richárd")
@@ -8,14 +11,27 @@ def nevjegy():
     gomb2.pack()
     abl2.mainloop()
 
+
+#felszín
+
 def felszin():
     def szamit():
         a = eval(mezo1.get())
         b = eval(mezo2.get())
         c = eval(mezo3.get())
-        felszin = 2*(a*b+a*c+b*c)
+        if a > 0 and b > 0 and c > 0:
+            felszin = 2*(a*b+a*c+b*c)
+            mezo4.delete(0, END)
+            mezo4.insert(0,str(felszin))
+        else:
+            mezo4.delete(0, END)
+            mezo4.insert(0, "Nem jó adat! (x < 0)")
+    
+    def torles():
+        mezo1.delete(0, END)
+        mezo2.delete(0, END)
+        mezo3.delete(0, END)
         mezo4.delete(0, END)
-        mezo4.insert(0,str(felszin))
 
     abl3 = Toplevel(foablak)
     abl3.title("Téglatest felszíne")
@@ -42,34 +58,51 @@ def felszin():
     mezo2.grid(row = 2, column = 2, sticky = W)
     mezo3.grid(row = 3, column = 2, sticky = W)
     mezo4.grid(row = 5, column = 2, sticky = W)
+
+    felszinkilep = Button(abl3, text = "Kilépés", command = abl3.destroy)
+    felszinkilep.grid(row = 6, column = 3, sticky = W)
+
+    felszin_adattorles = Button(abl3, text = "Törlés", command = torles)
+    felszin_adattorles.grid(row = 6, column = 1, sticky = W)
     
     abl3.mainloop()
 
 
+#térfogat
 
 def terfogat():
     def szamit():
         a = eval(mezo1.get())
         b = eval(mezo2.get())
         c = eval(mezo3.get())
-        terfogat = a*b*c
+        if a > 0 and b > 0 and c > 0:
+            terfogat = a*b*c
+            mezo4.delete(0, END)
+            mezo4.insert(0,str(terfogat))
+        else:
+            mezo4.delete(0, END)
+            mezo4.insert(0, "Nem jó adat! (x < 0)")
+
+    def torles():
+        mezo1.delete(0, END)
+        mezo2.delete(0, END)
+        mezo3.delete(0, END)
         mezo4.delete(0, END)
-        mezo4.insert(0,str(terfogat))
 
-    abl3 = Toplevel(foablak)
-    abl3.title("Téglatest térfogata")
-    abl3.minsize(width = 300, height = 100)
-    szoveg1 = Label(abl3, text = "a:")
-    szoveg2 = Label(abl3, text = "b:")
-    szoveg3 = Label(abl3, text = "c:")
-    szoveg4 = Label(abl3, text = "Eredmény:")
+    abl4 = Toplevel(foablak)
+    abl4.title("Téglatest térfogata")
+    abl4.minsize(width = 300, height = 100)
+    szoveg1 = Label(abl4, text = "a:")
+    szoveg2 = Label(abl4, text = "b:")
+    szoveg3 = Label(abl4, text = "c:")
+    szoveg4 = Label(abl4, text = "Eredmény:")
     
-    gomb1 = Button(abl3, text = "Számít", command= szamit)
+    gomb1 = Button(abl4, text = "Számít", command= szamit)
 
-    mezo1 = Entry(abl3)
-    mezo2 = Entry(abl3)
-    mezo3 = Entry(abl3)
-    mezo4 = Entry(abl3)
+    mezo1 = Entry(abl4)
+    mezo2 = Entry(abl4)
+    mezo3 = Entry(abl4)
+    mezo4 = Entry(abl4)
     szoveg1.grid(row = 1)
     szoveg2.grid(row = 2)
     szoveg3.grid(row = 3)
@@ -81,8 +114,17 @@ def terfogat():
     mezo2.grid(row = 2, column = 2, sticky = W)
     mezo3.grid(row = 3, column = 2, sticky = W)
     mezo4.grid(row = 5, column = 2, sticky = W)
+
+    terfogatkilep = Button(abl4, text = "Kilépés", command = abl4.destroy)
+    terfogatkilep.grid(row = 6, column = 3, sticky = W)
+
+    terfogat_adattorles = Button(abl4, text = "Törlés", command = torles)
+    terfogat_adattorles.grid(row = 6, column = 1, sticky = W)
     
-    abl3.mainloop()
+    abl4.mainloop()
+
+
+#foablak
 
 foablak = Tk()
 foablak.title("A téglatest adatai")
@@ -104,5 +146,8 @@ teglatest = Menu(menu2)
 teglatest.add_command(label = "Felszín", command = felszin, underline = 0)
 teglatest.add_command(label = "Térfogat", command = terfogat, underline = 0)
 menu2.config(menu = teglatest)
+
+gomb3 = Button(foablak, text = "Kilépés", command = foablak.destroy)
+gomb3.pack(side = BOTTOM)
 
 foablak.mainloop()
